60a3948aed3d24a5d78b11f6d41b3a24a9beeb0e
YacineSoussi <Yacine1.soussi@gmail.com>
2022-03-09 14:39:17 +0100

    login jwt

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
